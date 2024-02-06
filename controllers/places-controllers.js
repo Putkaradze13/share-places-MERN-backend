2394c4c5828bb83020dcac3eb06505aa1aa81efc
Tengiz Putkaradze <phutkaradze.13@gmail.com>
2024-02-06 19:11:50 +0400

    corrected app.js

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -45,7 +45,7 @@ const getPlacesByUserId = async (req, res, next) => {
     );
   }
 
-  if (!userWithPlaces) {
+  if (!userWithPlaces || userWithPlaces.places.length === 0) {
     return next(
       new HttpError("Could not find places for the provided user id.", 404)
     );
